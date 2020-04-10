de4e8798f04de2c4d79440f871c1392a36337b95
Dmitriy <sitesun707@gmail.com>
2020-04-10 16:01:12 +0300

    Задание 7

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 'use strict';
 
+let startBtn = document.getElementById("start"),
+	budgetValue = document.getElementsByClassName('budget-value')[0],
+	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+	levelValue = document.getElementsByClassName('level-value')[0],
+	expensesValue = document.getElementsByClassName('expenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesBtn = document.getElementsByTagName('button')[0],
+	optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start() {
-        money = +prompt ("Ваш бюджет на месяц?", "");
-        time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
 
-        while(isNaN(money) || money == "" || money == null) {
-            money = +prompt ("Ваш бюджет на месяц?", "");
-        }
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", ""); 
+    }
+
 }
 start();
-
+    
 let appData = {
     budget: money,
     timeData: time,
@@ -19,7 +44,7 @@ let appData = {
     optionalExpenses: {},
     income: [],
     savings: true,
-    chooseExpenses: function() {
+    chooseExpenses: function () {
         for (let i = 0; i < 2; i++) {
             let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
                 b = prompt ("Во сколько обойдется?", "");
@@ -28,48 +53,66 @@ let appData = {
         
                 console.log ("done");
         
-                this.expenses[a] = b;
-            } else {                            
+                appData.expenses[a] = b;
+            } else {
                 console.log ("bad result");
                 i--;
             }
         
-        };
+        }
     },
-    detectDayBudget: function() {
-        this.moneyPerDay = (this.budget / 30).toFixed();
-        alert ("Бюджет на 1 день составляет " + this.moneyPerDay + "руб.");
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
     },
-    detectLevel: function() {
-        if (this.moneyPerDay < 100) {
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100) {
             console.log ("Это минимальный уровень достатка!");
-        } else if (this.moneyPerDay > 100 && this.moneyPerDay < 2000) {
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
             console.log ("Это средний уровень достатка!");
-        } else if (this.moneyPerDay > 2000) {
+        } else if (appData.moneyPerDay > 2000) {
             console.log ("Это высокий уровень достатка!");
         } else {
-            console.log ("Произошла ошибка");
+            console.log ("Ошибочка...!");
         }
     },
-    checkSavings: function() {
-        if(this.savings == true) {
-            let save = +prompt("Какова сумма накоплений", ""),
-                percent = +prompt("Под какой процент", "");
+    checkSavings: function () {
+        if (appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
     
-                this.monthIncome = save/100/12*percent;
-            alert("Доход в месяц с вашего депозита: " + this.monthIncome);
+                appData.monthIncome = save/100/12*percent;
+                alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
         }
     },
-    chooseOptExpenses: function() {
-        for(let i = 1; i < 4; i++) {
-            let questionOptExpenses = prompt('Статья необязательных расходов?', '');
-            this.optionalExpenses[1] = questionOptExpenses;
+    chooseOptExpenses: function () {
+        for (let i = 1; i <= 3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов?");
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
         }
     },
-    chooseIncome: function(){
-        let items = prompt('что принесет дополнительный доход? (Перечислите через запятую)', '');
-        this.income = items.split(', ');
-        appData.income.push(prompt('Может что-то ещё?'));
-        appData.income.sort();
-    } 
+    chooseIncome: function () {
+
+        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
+
+        if (typeof(items) != "string" || items == "" || typeof(items) == null) {
+            console.log("Вы ввели некорректные данные или не ввели их вовсе");
+        } else {
+            appData.income = items.split(", ");
+            appData.income.push(prompt("Может что-то еще?"));
+            appData.income.sort();
+        }
+
+        appData.income.forEach (function (itemmassive, i) {
+            alert("Способы доп. заработка: " + (i+1) + " - " + itemmassive);
+        });
+
+    }
+
+
 };
+
+for (let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+}
